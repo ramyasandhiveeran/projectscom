44d7151f09fdce997cc10f73f83626677100cc67
ramyasandhiveeran <141127157+ramyasandhiveeran@users.noreply.github.com>
2023-08-01 15:01:52 +0530

    Add files via upload

---
+++ slackconnect.js
@@ -0,0 +1,12 @@
+  var good= document.getElementById('section12');
+
+
+  function handleclick(){
+    good.classList.add("active");
+  }
+
+  function handleclick1(){
+    good.classList.remove("active")
+  }
+
+  
